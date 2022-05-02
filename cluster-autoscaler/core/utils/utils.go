5a4c295b1c25f5f614868303f2ab55e0823e766d
Himanshu Sharma <himanshu.sharma10@sap.com>
2022-05-02 15:14:34 +0530

    spelling mistake corrected

--- cluster-autoscaler/core/utils/utils.go
+++ cluster-autoscaler/core/utils/utils.go
@@ -100,7 +100,7 @@ func GetNodeInfosForGroups(nodes []*apiv1.Node, nodeInfoCache map[string]*schedu
 		}
 
 		// No good template, check cache of previously running nodes(only when nodeGrp min size is not zero. This is to avoid scenarios where nodeGrp instanceType is updated,
-		// but still cached ol nodeTemplate is used)
+		// but still cached old nodeTemplate is used)
 		if nodeInfoCache != nil && nodeGroup.MinSize() != 0 {
 			if nodeInfo, found := nodeInfoCache[id]; found {
 				if nodeInfoCopy, err := deepCopyNodeInfo(nodeInfo); err == nil {
