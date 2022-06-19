30ad56b4b9d1b786c2f9478352ca8d97c3607eb2
Himanshu Sharma <himanshu.sharma10@sap.com>
2022-06-19 16:12:45 +0530

    docstring updated

--- cluster-autoscaler/cloudprovider/mcm/mcm_manager.go
+++ cluster-autoscaler/cloudprovider/mcm/mcm_manager.go
@@ -869,7 +869,8 @@ func buildGenericLabels(template *nodeTemplate, nodeName string) map[string]stri
 
 // isMachineTerminating returns true if machine is already being terminated or considered for termination by autoscaler.
 // The machine marked `Failed` are also considered terminating now because eventually they will be terminated. This prevents race b/w MCM
-// and CA logic
+// and CA logic. However, the logs after this log might suggest that machineDeployment was reduced in size and unregistered nodes were removed
+// but that is because we don't return error if this race condition happens.
 func isMachineTerminating(machine *v1alpha1.Machine) bool {
 	if !machine.GetDeletionTimestamp().IsZero() || machine.Status.CurrentStatus.Phase == v1alpha1.MachineFailed {
 		klog.Infof("Machine %q is already being terminated, and hence skipping the deletion", machine.Name)
