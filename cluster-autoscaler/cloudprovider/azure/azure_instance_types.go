62c76df79b12c9163adaba60cad6e494c828d66f
Himanshu Sharma <himanshu.sharma10@sap.com>
2022-06-24 10:25:23 +0530

    sync_file updates & other changes

--- cluster-autoscaler/cloudprovider/azure/azure_instance_types.go
+++ cluster-autoscaler/cloudprovider/azure/azure_instance_types.go
@@ -3244,12 +3244,6 @@ var InstanceTypes = map[string]*InstanceType{
 		MemoryMb:     458752,
 		GPU:          0,
 	},
-	"Standard_HB120rs_v3": {
-		InstanceType: "Standard_HB120rs_v3",
-		VCPU:         120,
-		MemoryMb:     458752,
-		GPU:          0,
-	},
 	"Standard_HB60rs": {
 		InstanceType: "Standard_HB60rs",
 		VCPU:         60,
