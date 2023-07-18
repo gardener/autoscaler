a2c4ff38083e7a7fff3696408a6db3435c7e5944
Rishabh Patel <rishabh.patel@sap.com>
2023-07-18 15:18:39 +0530

    introduce unit tests for DeleteNodes method

--- cluster-autoscaler/cloudprovider/mcm/test_utils.go
+++ cluster-autoscaler/cloudprovider/mcm/test_utils.go
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2017 SAP SE or an SAP affiliate company. All rights reserved.
+Copyright (c) 2023 SAP SE or an SAP affiliate company. All rights reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -20,74 +20,36 @@ import (
 	"fmt"
 	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider"
 	"k8s.io/client-go/tools/cache"
+	"testing"
 	"time"
 
-	. "github.com/onsi/gomega"
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/util/intstr"
-	coreinformers "k8s.io/client-go/informers"
-	"k8s.io/utils/pointer"
-
-	machine_internal "github.com/gardener/machine-controller-manager/pkg/apis/machine"
+	machineinternal "github.com/gardener/machine-controller-manager/pkg/apis/machine"
 	"github.com/gardener/machine-controller-manager/pkg/apis/machine/v1alpha1"
 	faketyped "github.com/gardener/machine-controller-manager/pkg/client/clientset/versioned/typed/machine/v1alpha1/fake"
 	machineinformers "github.com/gardener/machine-controller-manager/pkg/client/informers/externalversions"
 	mcmcache "github.com/gardener/machine-controller-manager/pkg/util/provider/cache"
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	customfake "k8s.io/autoscaler/cluster-autoscaler/cloudprovider/mcm/fakeclient"
+	deletetaint "k8s.io/autoscaler/cluster-autoscaler/utils/taints"
+	coreinformers "k8s.io/client-go/informers"
 )
-
-//func TestMachineControllerManagerSuite(t *testing.T) {
-//	// for filtering out warning logs. Reflector short watch warning logs won't print now
-//	klog.SetOutput(io.Discard)
-//	flags := &flag.FlagSet{}
-//	klog.InitFlags(flags)
-//	if err := flags.Set("logtostderr", "false"); err != nil {
-//		t.Errorf("failed to set flags: %v", err)
-//	}
-//
-//	RegisterFailHandler(Fail)
-//	RunSpecs(t, "Machine Controller Manager Suite")
-//}
 
 var (
-	TestMachineClass = "machineClass-0"
-	testNamespace    = "test-namespace"
+	testNamespace         = "test-namespace"
+	priorityAnnotationKey = "machinepriority.machine.sapcloud.io"
+	testTaintValue        = fmt.Sprint(time.Now().Unix())
 )
-
-func newMachineDeployment(
-	specTemplate *v1alpha1.MachineTemplateSpec,
-	replicas int32,
-	minReadySeconds int32,
-	maxSurge int,
-	maxUnavailable int,
-	statusTemplate *v1alpha1.MachineDeploymentStatus,
-	owner *metav1.OwnerReference,
-	annotations map[string]string,
-	labels map[string]string,
-) *v1alpha1.MachineDeployment {
-	md := newMachineDeployments(1, specTemplate, replicas, minReadySeconds, statusTemplate, owner, annotations, labels)[0]
-	intStrMaxSurge := intstr.FromInt(maxSurge)
-	intStrMaxUnavailable := intstr.FromInt(maxUnavailable)
-	md.Spec.Strategy.RollingUpdate.MaxSurge = &intStrMaxSurge
-	md.Spec.Strategy.RollingUpdate.MaxUnavailable = &intStrMaxUnavailable
-
-	return md
-}
 
 func newMachineDeployments(
 	machineDeploymentCount int,
-	specTemplate *v1alpha1.MachineTemplateSpec,
 	replicas int32,
-	minReadySeconds int32,
 	statusTemplate *v1alpha1.MachineDeploymentStatus,
-	owner *metav1.OwnerReference,
 	annotations map[string]string,
 	labels map[string]string,
 ) []*v1alpha1.MachineDeployment {
-
-	intStr1 := intstr.FromInt(1)
 	machineDeployments := make([]*v1alpha1.MachineDeployment, machineDeploymentCount)
 	for i := range machineDeployments {
 		machineDeployment := &v1alpha1.MachineDeployment{
@@ -96,68 +58,28 @@ func newMachineDeployments(
 				Kind:       "MachineDeployment",
 			},
 			ObjectMeta: metav1.ObjectMeta{
-				Name:      fmt.Sprintf("machinedeployment-%d", i),
+				Name:      fmt.Sprintf("machinedeployment-%d", i+1),
 				Namespace: testNamespace,
 				Labels:    labels,
 			},
 			Spec: v1alpha1.MachineDeploymentSpec{
-				MinReadySeconds: minReadySeconds,
-				Replicas:        replicas,
-				Selector: &metav1.LabelSelector{
-					MatchLabels: deepCopy(specTemplate.ObjectMeta.Labels),
-				},
-				Strategy: v1alpha1.MachineDeploymentStrategy{
-					Type: v1alpha1.RollingUpdateMachineDeploymentStrategyType,
-					RollingUpdate: &v1alpha1.RollingUpdateMachineDeployment{
-						MaxSurge:       &intStr1,
-						MaxUnavailable: &intStr1,
-					},
-				},
-				Template: *specTemplate.DeepCopy(),
+				Replicas: replicas,
 			},
 		}
-
 		if statusTemplate != nil {
 			machineDeployment.Status = *statusTemplate.DeepCopy()
 		}
-
-		if owner != nil {
-			machineDeployment.OwnerReferences = append(machineDeployment.OwnerReferences, *owner.DeepCopy())
-		}
-
 		if annotations != nil {
 			machineDeployment.Annotations = annotations
 		}
-
 		machineDeployments[i] = machineDeployment
 	}
 	return machineDeployments
 }
 
-func newMachineSet(
-	specTemplate *v1alpha1.MachineTemplateSpec,
-	name string,
-	replicas int32,
-	minReadySeconds int32,
-	statusTemplate *v1alpha1.MachineSetStatus,
-	owner *metav1.OwnerReference,
-	annotations map[string]string,
-	labels map[string]string,
-) *v1alpha1.MachineSet {
-	ms := newMachineSets(1, specTemplate, replicas, minReadySeconds, statusTemplate, owner, annotations, labels)[0]
-	ms.Name = name
-	return ms
-}
-
 func newMachineSets(
 	machineSetCount int,
-	specTemplate *v1alpha1.MachineTemplateSpec,
-	replicas int32,
-	minReadySeconds int32,
-	statusTemplate *v1alpha1.MachineSetStatus,
-	owner *metav1.OwnerReference,
-	annotations map[string]string,
-	labels map[string]string,
+	mdName string,
 ) []*v1alpha1.MachineSet {
 
 	machineSets := make([]*v1alpha1.MachineSet, machineSetCount)
@@ -168,106 +90,38 @@ func newMachineSets(
 				Kind:       "MachineSet",
 			},
 			ObjectMeta: metav1.ObjectMeta{
-				Name:      fmt.Sprintf("machineset-%d", i),
-				Namespace: testNamespace,
-				Labels:    labels,
-			},
-			Spec: v1alpha1.MachineSetSpec{
-				MachineClass:    *specTemplate.Spec.Class.DeepCopy(),
-				MinReadySeconds: minReadySeconds,
-				Replicas:        replicas,
-				Selector: &metav1.LabelSelector{
-					MatchLabels: deepCopy(specTemplate.ObjectMeta.Labels),
-				},
-				Template: *specTemplate.DeepCopy(),
+				Name:            fmt.Sprintf("machineset-%d", i+1),
+				Namespace:       testNamespace,
+				OwnerReferences: []metav1.OwnerReference{{Name: mdName}},
 			},
 		}
-
-		if statusTemplate != nil {
-			ms.Status = *statusTemplate.DeepCopy()
-		}
-
-		if owner != nil {
-			ms.OwnerReferences = append(ms.OwnerReferences, *owner.DeepCopy())
-		}
-
-		if annotations != nil {
-			ms.Annotations = annotations
-		}
-
 		machineSets[i] = ms
 	}
 	return machineSets
 }
 
-func deepCopy(m map[string]string) map[string]string {
-	r := make(map[string]string, len(m))
-	for k := range m {
-		r[k] = m[k]
-	}
-	return r
-}
-
-func newMachinesFromMachineSet(
-	machineCount int,
-	machineSet *v1alpha1.MachineSet,
-	statusTemplate *v1alpha1.MachineStatus,
-	annotations map[string]string,
-	labels map[string]string,
-) []*v1alpha1.Machine {
-	t := &machineSet.TypeMeta
-
-	finalLabels := make(map[string]string, 0)
-	for k, v := range labels {
-		finalLabels[k] = v
-	}
-	for k, v := range machineSet.Spec.Template.Labels {
-		finalLabels[k] = v
-	}
-
-	return newMachines(
-		machineCount,
-		&machineSet.Spec.Template,
-		statusTemplate,
-		&metav1.OwnerReference{
-			APIVersion:         t.APIVersion,
-			Kind:               t.Kind,
-			Name:               machineSet.Name,
-			UID:                machineSet.UID,
-			BlockOwnerDeletion: pointer.BoolPtr(true),
-			Controller:         pointer.BoolPtr(true),
-		},
-		annotations,
-		finalLabels,
-	)
-}
-
 func newMachine(
-	specTemplate *v1alpha1.MachineTemplateSpec,
+	name string,
+	providerIdGenerateName string,
 	statusTemplate *v1alpha1.MachineStatus,
-	owner *metav1.OwnerReference,
-	annotations map[string]string,
-	labels map[string]string,
+	mdName, msName string,
+	priorityAnnotationValue string,
+	setDeletionTimeStamp bool,
 ) *v1alpha1.Machine {
-	return newMachines(1, specTemplate, statusTemplate, owner, annotations, labels)[0]
+	m := newMachines(1, providerIdGenerateName, statusTemplate, mdName, msName, []string{priorityAnnotationValue}, []bool{setDeletionTimeStamp})[0]
+	m.Name = name
+	return m
 }
 
 func newMachines(
 	machineCount int,
-	specTemplate *v1alpha1.MachineTemplateSpec,
+	providerIdGenerateName string,
 	statusTemplate *v1alpha1.MachineStatus,
-	owner *metav1.OwnerReference,
-	annotations map[string]string,
-	labels map[string]string,
+	mdName, msName string,
+	priorityAnnotationValues []string,
+	setDeletionTimeStamp []bool,
 ) []*v1alpha1.Machine {
 	machines := make([]*v1alpha1.Machine, machineCount)
-
-	if annotations == nil {
-		annotations = make(map[string]string, 0)
-	}
-	if labels == nil {
-		labels = make(map[string]string, 0)
-	}
 
 	currentTime := metav1.Now()
 
@@ -278,85 +132,75 @@ func newMachines(
 				Kind:       "Machine",
 			},
 			ObjectMeta: metav1.ObjectMeta{
-				Name:              fmt.Sprintf("machine-%d", i),
-				Namespace:         testNamespace,
-				Labels:            labels,
-				Annotations:       annotations,
+				Name:      fmt.Sprintf("machine-%d", i+1),
+				Namespace: testNamespace,
+				OwnerReferences: []metav1.OwnerReference{
+					{Name: msName},
+				},
+				Labels:            map[string]string{"name": mdName},
+				Annotations:       map[string]string{priorityAnnotationKey: priorityAnnotationValues[i]},
 				CreationTimestamp: metav1.Now(),
-				DeletionTimestamp: &currentTime,
 			},
-			Spec: *newMachineSpec(&specTemplate.Spec, i),
+			Spec: v1alpha1.MachineSpec{ProviderID: fmt.Sprintf("%s/i%d", providerIdGenerateName, i+1)},
 		}
 
+		m.Labels["node"] = fmt.Sprintf("node-%d", i+1)
+		if setDeletionTimeStamp[i] {
+			m.ObjectMeta.DeletionTimestamp = &currentTime
+		}
 		if statusTemplate != nil {
 			m.Status = *newMachineStatus(statusTemplate)
 		}
-
-		if owner != nil {
-			m.OwnerReferences = append(m.OwnerReferences, *owner.DeepCopy())
-		}
-
 		machines[i] = m
 	}
 	return machines
 }
 
 func newNode(
-	_ int,
-	nodeSpec *corev1.NodeSpec,
-	nodeStatus *corev1.NodeStatus,
+	nodeName,
+	providerId string,
+	addToBeDeletedTaint bool,
 ) *corev1.Node {
-	return newNodes(1, nodeSpec, nodeStatus)[0]
+	node := newNodes(1, providerId, []bool{addToBeDeletedTaint})[0]
+	clone := node.DeepCopy()
+	clone.Name = nodeName
+	clone.Spec.ProviderID = providerId
+	return clone
 }
 
 func newNodes(
 	nodeCount int,
-	nodeSpec *corev1.NodeSpec,
-	_ *corev1.NodeStatus,
+	providerIdGenerateName string,
+	addToBeDeletedTaint []bool,
 ) []*corev1.Node {
 
 	nodes := make([]*corev1.Node, nodeCount)
 	for i := range nodes {
+		var taints []corev1.Taint
+		if addToBeDeletedTaint[i] {
+			taints = append(taints, corev1.Taint{
+				Key:    deletetaint.ToBeDeletedTaint,
+				Value:  testTaintValue,
+				Effect: corev1.TaintEffectNoSchedule,
+			})
+		}
 		node := &corev1.Node{
 			TypeMeta: metav1.TypeMeta{
 				APIVersion: "v1",
 				Kind:       "Node",
 			},
 			ObjectMeta: metav1.ObjectMeta{
-				Name: fmt.Sprintf("node-%d", i),
+				Name: fmt.Sprintf("node-%d", i+1),
 			},
-			Spec: *nodeSpec.DeepCopy(),
+			Spec: corev1.NodeSpec{
+				ProviderID: fmt.Sprintf("%s/i%d", providerIdGenerateName, i+1),
+				Taints:     taints,
+			},
 		}
 
 		nodes[i] = node
 	}
 	return nodes
-}
-
-func newObjectMeta(meta *metav1.ObjectMeta, index int) *metav1.ObjectMeta {
-	r := meta.DeepCopy()
-
-	if r.Name != "" {
-		return r
-	}
-
-	if r.GenerateName != "" {
-		r.Name = fmt.Sprintf("%s-%d", r.GenerateName, index)
-		return r
-	}
-
-	r.Name = fmt.Sprintf("machine-%d", index)
-	return r
-}
-
-func newMachineSpec(specTemplate *v1alpha1.MachineSpec, index int) *v1alpha1.MachineSpec {
-	r := specTemplate.DeepCopy()
-	if r.ProviderID == "" {
-		return r
-	}
-
-	r.ProviderID = fmt.Sprintf("%s-%d", r.ProviderID, index)
-	return r
 }
 
 func newMachineStatus(statusTemplate *v1alpha1.MachineStatus) *v1alpha1.MachineStatus {
@@ -368,11 +212,12 @@ func newMachineStatus(statusTemplate *v1alpha1.MachineStatus) *v1alpha1.MachineS
 }
 
 func createMcmManager(
+	t *testing.T,
 	stop <-chan struct{},
 	namespace string,
 	nodeGroups []string, controlMachineObjects, targetCoreObjects []runtime.Object,
 ) (*McmManager, *customfake.FakeObjectTrackers, []cache.InformerSynced) {
-
+	g := NewWithT(t)
 	fakeControlMachineClient, controlMachineObjectTracker := customfake.NewMachineClientSet(controlMachineObjects...)
 	fakeTypedMachineClient := &faketyped.FakeMachineV1alpha1{
 		Fake: &fakeControlMachineClient.Fake,
@@ -409,8 +254,8 @@ func createMcmManager(
 	machineClasses := machineSharedInformers.MachineClasses()
 
 	internalExternalScheme := runtime.NewScheme()
-	Expect(machine_internal.AddToScheme(internalExternalScheme)).To(Succeed())
-	Expect(v1alpha1.AddToScheme(internalExternalScheme)).To(Succeed())
+	g.Expect(machineinternal.AddToScheme(internalExternalScheme)).To(Succeed())
+	g.Expect(v1alpha1.AddToScheme(internalExternalScheme)).To(Succeed())
 
 	mcmManager := McmManager{
 		namespace: namespace,
@@ -424,6 +269,8 @@ func createMcmManager(
 		machineLister:           machines.Lister(),
 		machineClassLister:      machineClasses.Lister(),
 		nodeLister:              nodes.Lister(),
+		maxRetryTimeout:         5 * time.Second,
+		conflictRetryInterval:   1 * time.Second,
 	}
 
 	hasSyncedCachesFns := []cache.InformerSynced{
@@ -437,6 +284,7 @@ func createMcmManager(
 	return &mcmManager, fakeObjectTrackers, hasSyncedCachesFns
 }
 
-func waitForCacheSync(stop <-chan struct{}, hasSyncedCachesFns []cache.InformerSynced) {
-	Expect(mcmcache.WaitForCacheSync(stop, hasSyncedCachesFns...)).To(BeTrue())
+func waitForCacheSync(t *testing.T, stop <-chan struct{}, hasSyncedCachesFns []cache.InformerSynced) {
+	g := NewWithT(t)
+	g.Expect(mcmcache.WaitForCacheSync(stop, hasSyncedCachesFns...)).To(BeTrue())
 }
