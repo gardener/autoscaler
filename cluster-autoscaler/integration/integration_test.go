fa02eff2404ff2cb39b2e0e25313a33002105e25
elankath <tarun.ramakrishna.elankath@sap.com>
2023-07-18 11:19:58 +0530

    enabled volume test for GCP scale-up from zero

--- cluster-autoscaler/integration/integration_test.go
+++ cluster-autoscaler/integration/integration_test.go
@@ -251,7 +251,7 @@ func (driver *Driver) controllerTests() {
 				By("Creating StorageClass with topology restrictions")
 				class := "myclass"
 				provider := os.Getenv("CLUSTER_PROVIDER")
-				if provider != "aws" {
+				if provider != "aws" && provider != "gcp" {
 					return
 				}
 				//TODO: support this testcase for GKE once nodeTemplate in machinedeployment has the label `topology.gke.io/zone`: <zone-name>
