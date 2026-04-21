105b72acf23bf87c08fa8e85ded00e8a7c5a429d
thiyyakat <meghana.thiyyakat@sap.com>
2026-04-21 14:09:49 +0530

    Fix tests to expect sentinel error when no node is deleted

--- cluster-autoscaler/cloudprovider/mcm/mcm_cloud_provider_test.go
+++ cluster-autoscaler/cloudprovider/mcm/mcm_cloud_provider_test.go
@@ -217,7 +217,7 @@ func TestDeleteNodes(t *testing.T) {
 			expect{
 				mdName:     "machinedeployment-1",
 				mdReplicas: 2,
-				err:        nil,
+				err:        ErrNoNodesToDelete,
 			},
 		},
 		{
@@ -233,7 +233,7 @@ func TestDeleteNodes(t *testing.T) {
 			expect{
 				mdName:     "machinedeployment-1",
 				mdReplicas: 2,
-				err:        nil,
+				err:        ErrNoNodesToDelete,
 			},
 		},
 		{
@@ -346,7 +346,7 @@ func TestForceDeleteNodes(t *testing.T) {
 			expect{
 				mdName:     "machinedeployment-1",
 				mdReplicas: 1,
-				err:        nil,
+				err:        ErrNoNodesToDelete,
 			},
 		},
 		{
